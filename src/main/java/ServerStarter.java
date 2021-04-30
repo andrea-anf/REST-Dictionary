1dddaf9a2768d301df21e4e90e0bddb1aabc6e56
Susara-X- <anfuso.studio@gmail.com>
2021-04-30 19:09:34 +0200

    Starting project

---
+++ src/main/java/ServerStarter.java
@@ -0,0 +1,5 @@
+public class ServerStarter {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Word.java
@@ -0,0 +1,25 @@
+public class Word {
+    private String word;
+    private String definition;
+
+    public Word(String word, String definition){
+        this.word = word;
+        this.definition = definition;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+}
